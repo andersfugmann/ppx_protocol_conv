9bebf94f4c1e99ec2033d844055089f80d12cfb6
Anders Fugmann <anders@fugmann.net>
2017-11-20 09:43:52 +0100

    Add tests

--- test/util.ml
+++ test/util.ml
@@ -1,5 +1,5 @@
 let test_json name to_json of_json t =
-  Printf.printf "%s: %!" name;
+  Printf.printf "json %s: %!" name;
   let json = to_json t in
   Printf.printf "%s%!" (Yojson.Safe.to_string json);
   assert (Base.Poly.equal t (of_json json));
@@ -7,8 +7,14 @@ let test_json name to_json of_json t =
 
 
 let test_xml name to_xml of_xml t =
-  Printf.printf "%s: %!" name;
+  Printf.printf "xml: %s: %!" name;
   let xml = to_xml t in
   Printf.printf "%s%!" (Xml.to_string (Xml.Element ("Root node", [] , xml)));
   assert (Base.Poly.equal t (of_xml xml));
+  Printf.printf " - Ok\n"
+
+let test_msgpack name to_msgpack of_msgpack t =
+  Printf.printf "msgpack: %s: %!" name;
+  let msgpack = to_msgpack t in
+  assert (Base.Poly.equal t (of_msgpack msgpack));
   Printf.printf " - Ok\n"
