efd7635b1997a756d9c0ccc8a4beebe07017284a
Anders Fugmann <anders@fugmann.net>
2019-04-01 22:43:51 +0200

    Move tests around a bit

---
+++ drivers/json/test/test_expect.ml
@@ -0,0 +1,139 @@
+open Protocol_conv_json
+module Default_parameters = Ppx_protocol_driver.Default_parameters
+module Make = Json.Make
+
+(** Test parameters. *)
+module Test = struct
+  module Standard = struct
+    module Json = Make(Default_parameters)
+    type u = A | B of int
+    and t = {
+      int: int;
+      u: u;
+      uu: u;
+    } [@@deriving protocol ~driver:(module Json)]
+    let t = { int = 5; u = A; uu = B 5 }
+    let%test _ =  t |> to_json |> of_json = t
+    let%expect_test _ =
+      let s = Json.to_string_hum (to_json t) in
+      print_endline s;
+      [%expect {| { "uu": [ "B", 5 ], "u": "A", "int": 5 } |}]
+  end
+
+  module Field_upper = struct
+    module Json = Make(
+      struct
+        include Default_parameters
+        let field_name name = "F_" ^ name
+        let variant_name name = "V_" ^ name
+      end)
+    type u = A | B of int
+    and t = {
+      int: int;
+      u: u;
+      uu: u;
+      v: [ `A of int ]
+    } [@@deriving protocol ~driver:(module Json)]
+
+    let t = { int = 5; u = A; uu = B 5; v = `A 6 }
+    let%test _ =  t |> to_json |> of_json = t
+    let%expect_test _ =
+      let s = Json.to_string_hum (to_json t) in
+      print_endline s;
+      [%expect {| { "F_v": [ "V_A", 6 ], "F_uu": [ "V_B", 5 ], "F_u": "V_A", "F_int": 5 } |}]
+  end
+
+  module Singleton_as_list = struct
+    module Json = Make(
+      struct
+        include Default_parameters
+        let singleton_constr_as_string = false
+      end)
+    type u = A | B of int
+    and t = {
+      int: int;
+      u: u;
+      uu: u;
+      v: [ `X | `Y of int ];
+    } [@@deriving protocol ~driver:(module Json)]
+
+    let t = { int = 5; u = A; uu = B 5; v = `X }
+    let%test _ =  t |> to_json |> of_json = t
+    let%expect_test _ =
+      let s = Json.to_string_hum (to_json t) in
+      print_endline s;
+      [%expect {| { "v": [ "X" ], "uu": [ "B", 5 ], "u": [ "A" ], "int": 5 } |}]
+  end
+
+  module Omit_default = struct
+    module Json = Make(
+      struct
+        include Default_parameters
+        let omit_default_values = true
+      end)
+    type u = A | B of int
+    and t = {
+      int: int; [@default 5]
+      u: u;
+      uu: u;
+    } [@@deriving protocol ~driver:(module Json)]
+
+    let t = { int = 5; u = A; uu = B 5 }
+    let%test _ =  t |> to_json |> of_json = t
+    let%expect_test _ =
+      let s = Json.to_string_hum (to_json t) in
+      print_endline s;
+      [%expect {| { "uu": [ "B", 5 ], "u": "A" } |}]
+  end
+
+  module Keep_default = struct
+    module Json = Make(
+      struct
+        include Default_parameters
+        let omit_default_values = false
+      end)
+
+    type u = A | B of int
+    and t = {
+      int: int; [@default 5]
+      u: u;
+      uu: u;
+    } [@@deriving protocol ~driver:(module Json)]
+
+    let t = { int = 5; u = A; uu = B 5 }
+    let%test _ =  t |> to_json |> of_json = t
+    let%expect_test _ =
+      let s = Json.to_string_hum (to_json t) in
+      print_endline s;
+      [%expect {| { "uu": [ "B", 5 ], "u": "A" } |}]
+  end
+
+  module Field_name_count = struct
+    let count = ref 0
+    module Json = Make(
+      struct
+        include Default_parameters
+        let field_name name = incr count; name
+      end)
+    type u = A of { x:int; y: int; z:int } | B
+    [@@deriving protocol ~driver:(module Json)]
+    type t = {
+      a: int;
+      b: int;
+      c: u;
+    }
+    [@@deriving protocol ~driver:(module Json)]
+
+    let expect = !count
+    let t = { a=5; b=5; c=B }
+    let%test _ =  t |> to_json |> of_json = t
+    let%expect_test _ =
+      let c1 = !count in
+      let _ = t |> to_json |> of_json in
+      let c2 = !count in
+      let _ = t |> to_json |> of_json in
+      let c3 = !count in
+      Printf.printf "%d -> %d -> %d -> %d" expect c1 c2 c3;
+      [%expect {| 12 -> 12 -> 12 -> 12 |}]
+  end
+end
